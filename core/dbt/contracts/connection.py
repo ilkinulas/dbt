8259b538a8093419bd7d2ad102c06bc6e2a84548
ilkin Balkanay <ilkinulas@gmail.com>
2020-03-21 01:26:31 +0300

    jsonschema validation error for type Optional[Union[QueryComment, str, NoValue]]

--- core/dbt/contracts/connection.py
+++ core/dbt/contracts/connection.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass, field
 from typing import (
     Any, ClassVar, Dict, Tuple, Iterable, Optional, NewType, List, Callable,
     Union)
+
+from dbt.helper_types import NoValue
 from typing_extensions import Protocol
 
 from hologram import JsonSchemaMixin
@@ -201,6 +203,6 @@ class QueryComment(JsonSchemaMixin):
 
 class AdapterRequiredConfig(HasCredentials, Protocol):
     project_name: str
-    query_comment: Optional[Union[QueryComment, str]]
+    query_comment: Union[QueryComment, str, NoValue]
     cli_vars: Dict[str, Any]
     target_path: str
